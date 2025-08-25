fb9726b78988eff48f9ba257eac7590cfa8169b9
ankug07 <ankit.bti1@email.com>
2025-08-25 17:08:58 +0530

    files are commited

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "azurerm_resource_group" "a1" {
+  name = "ankit_rg"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "a2" {
+  name = "ajay_rg"
+  location = "eastus"
+}
+
+resource "azurerm_resource_group" "a3" {
+  name = "jai_rg"
+  location = "eastus"
+}
